29764ef93b9994d85d52ca7246c74fb5e0e87382
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-04-23 00:25:37 +0100

    Added launch files and dependency fixes.
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

--- robot_model/launch/launch_robot.launch.py
+++ robot_model/launch/launch_robot.launch.py
@@ -8,7 +8,7 @@ import os
 import yaml
 from ament_index_python.packages import get_package_share_path
 from launch_ros.actions import PushRosNamespace
-from launch.actions import GroupAction
+from launch.actions import GroupAction, ExecuteProcess
 
 def load_config_section(config_file, section):
     with open(config_file, 'r') as file:
@@ -22,11 +22,25 @@ def generate_launch_description():
     map_file = os.path.join(get_package_share_path('robot_model'),'map', 'map.yaml')
     bringup_launch_file = os.path.join("/opt/ros/humble/share/nav2_bringup/launch", "bringup_launch.py")
 
-    robot_namespace = "robot_" + os.getenv('HOSTNAME')
+    robot_namespace = ""
     robot_description = ParameterValue(
     Command(['xacro ', urdf_path]),
     value_type=str
 )
+
+    # Start Gazebo
+    gazebo = ExecuteProcess(
+        cmd=['gazebo', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+        output='screen'
+    )
+
+    # Spawn robot
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'my_robot', '-topic', '/robot_description'],
+        output='screen'
+    )
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
@@ -34,11 +48,11 @@ def generate_launch_description():
         namespace=robot_namespace,
         parameters=[{'robot_description': robot_description},
                     {'use_sim_time': True},],
-        remappings=[
-            ('/tf', f'/{robot_namespace}/tf'),
-            ('/tf_static', f'/{robot_namespace}/tf_static'),
-            ('robot_description', 'visualization')
-        ],
+        # remappings=[
+        #     ('/tf', f'/{robot_namespace}/tf'),
+        #     ('/tf_static', f'/{robot_namespace}/tf_static'),
+        #     ('robot_description', 'visualization')
+        # ],
     )
 
     joint_state_publisher_node = Node(
@@ -53,14 +67,14 @@ def generate_launch_description():
         executable="ros2_control_node",
         namespace=robot_namespace,
         parameters=[
-            {'robot_description': robot_description},
+            # {'robot_description': robot_description},
             os.path.join(get_package_share_path('robot_model'), 'config', 'ros2_control.yaml'),
             {'use_sim_time': True}
         ],
-        remappings=[
-            ('/tf', f'/{robot_namespace}/tf'),
-            ('/tf_static', f'/{robot_namespace}/tf_static')
-        ],
+        # remappings=[
+        #     ('/tf', f'/{robot_namespace}/tf'),
+        #     ('/tf_static', f'/{robot_namespace}/tf_static')
+        # ],
         output="screen"
     )
 
@@ -94,10 +108,12 @@ def generate_launch_description():
     ])
 
     return LaunchDescription([
+        gazebo,
+        spawn_entity,
         robot_state_publisher_node,
         joint_state_publisher_node,
         controller_manager_node,
         joint_state_spawner,
         diff_drive_spawner,
-        nav2
+        # nav2
     ])
